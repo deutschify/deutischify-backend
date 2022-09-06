a81294b4b16e8433ba7390868a45e1ed818bb377
Mez-A <dimoabr@gmail.com>
2022-09-06 10:22:58 +0200

    login works

---
+++ build/models.js
@@ -0,0 +1,24 @@
+export const getUsers = () => {
+    return [
+        {
+            email: "hendrick@gmail.com",
+            password: "123d",
+            accessGroups: ["loggedInUsers", "members"],
+        },
+        {
+            email: "anonymousUser",
+            password: "Anonymous",
+            accessGroups: ["loggedOutUsers"],
+        },
+        {
+            email: "hd@gmail.com",
+            password: "Hendrick",
+            accessGroups: ["loggedInUsers", "members"],
+        },
+        {
+            email: "an@gmail.com",
+            password: "Andrea",
+            accessGroups: ["loggedInUsers", "members"],
+        },
+    ];
+};
--- build/server.js
+++ build/server.js
@@ -1,50 +1,28 @@
 import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-
 import session from "express-session";
 import cookieParser from "cookie-parser";
-
 import { getUsers } from "./models.js";
 const users = getUsers();
-
-dotenv.config();
-
+// dotenv.config();
 const app = express();
-app.use(cors());
+// app.use(cors());
 const PORT = process.env.PORT || 8000;
-
 app.use(express.json());
-
-
-declare module 'express-session' {
-    export interface SessionData {
-        user: { [key: string]: any };
-    }
-}
-
-app.use(
-    session({
-        resave: true,
-        saveUninitialized: true,
-        secret: "tempsecret",
-    })
-);
-
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: "tempsecret",
+}));
 app.use(cookieParser());
-
-app.get("/", (req: express.Request, res: express.Response) => {
+app.get("/", (req, res) => {
     res.send("<h1>Deutschify Backend</h1>");
 });
-
-app.get("/users", (req: express.Request, res: express.Response) => {
+app.get("/users", (req, res) => {
     res.send(users);
 });
-
 // functions for loging in and out
 const loginSecondsMax = 10;
- 
-const logAnonymousUserIn = (req: express.Request, res: express.Response) => {
+const logAnonymousUserIn = (req, res) => {
     const user = users.find((user) => user.email === 'anonymousUser');
     if (user) {
         req.session.user = user;
@@ -53,12 +31,12 @@ const logAnonymousUserIn = (req: express.Request, res: express.Response) => {
         res.send({
             "currentUser": user
         });
-    } else {
+    }
+    else {
         res.status(500).send('bad login');
     }
-}
-
-const logUserIn = (email: string, password: string, req: express.Request, res: express.Response) => {
+};
+const logUserIn = (email, password, req, res) => {
     let user = users.find((user) => user.email === email && user.password === password);
     if (user) {
         req.session.user = user;
@@ -67,36 +45,32 @@ const logUserIn = (email: string, password: string, req: express.Request, res: e
         res.send({
             "currentUser": user
         });
-    } else {
+    }
+    else {
         // res.status(500).send('bad login');
         // res.send("Check your email and password");
         logAnonymousUserIn(req, res);
     }
-}
-
-app.post("/login", (req: express.Request, res: express.Response) => {
+};
+app.post("/login", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
     logUserIn(email, password, req, res);
-})
-
-app.get('/current-user', (req: express.Request, res: express.Response) => {
+});
+app.get('/current-user', (req, res) => {
     const user = req.session.user;
     if (user) {
         res.send({
             "currentUser": user
         });
-    } else {
+    }
+    else {
         logAnonymousUserIn(req, res);
     }
 });
-
-app.get('/logout', (req: express.Request, res: express.Response) => {
-    logAnonymousUserIn(req,res);
+app.get('/logout', (req, res) => {
+    logAnonymousUserIn(req, res);
 });
-
 app.listen(PORT, () => {
     console.log(`Listening on port http://localhost:${PORT}`);
 });
-
-
