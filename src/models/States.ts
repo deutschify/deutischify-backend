1176296be8bcf0f24f27ef036886f2a7b7a88140
Mez-A <dimoabr@gmail.com>
2022-09-13 10:41:27 +0200

    bundesländer works fine

---
+++ src/models/States.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const stateSchema = new mongoose.Schema({
+    number: String,
+    category: String,
+    question: String,
+    answerA: String,
+    answerB: String,
+    answerC: String,
+    answerD: String,
+    correctAnswer: String,
+});
+
+export const Berlin = mongoose.model("berlin-question", stateSchema);
+export const Hessen = mongoose.model("hessen-question", stateSchema);
+export const Sachsen = mongoose.model("sachsen-question", stateSchema);
+export const SachsenAnhalt = mongoose.model("sachsen-anhalt-question", stateSchema);
+export const SchleswigHolzstein = mongoose.model("schleswig-holzstein-question", stateSchema);
+export const Thuringen = mongoose.model("thuringen-question", stateSchema);
+export const Saarland = mongoose.model("saarland-question", stateSchema);
+export const Niedersachsen = mongoose.model("niedersachsen-question", stateSchema);
+export const MecklenburgVorpommern = mongoose.model("mecklenburg-vorpommern-question", stateSchema);
+export const Bayern = mongoose.model("bayern-question", stateSchema);
+export const Brandenburg = mongoose.model("brandenburg-question", stateSchema);
+export const Bremen = mongoose.model("bremen-question", stateSchema);
+export const Hamburg = mongoose.model("hamburg-question", stateSchema);
+export const NordrheinWestfallen = mongoose.model("nordrhein-westfallen-question", stateSchema);
+export const RheinlandPfalz = mongoose.model("rheinlandPfalz-question", stateSchema);
+export const BadenWurttemberg = mongoose.model("badenwurttemberg-question", stateSchema);
+export const Deutschland = mongoose.model("deutschland-question", stateSchema);
