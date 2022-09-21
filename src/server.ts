8354ac295257e290471dda5827251de032c9aff5
Omar-Tabaa <omar.tabaa90@gmail.com>
2022-09-22 00:48:14 +0200

    get a user

--- src/server.ts
+++ src/server.ts
@@ -326,6 +326,59 @@ app.post(
     }
 );
 
+//Update User Info
+
+app.put("/users/:_id", async (req: express.Request, res: express.Response) => {
+    if (req.params._id === req.body._id || req.body.isAdmin) {
+        if (req.body.password) {
+            try {
+                const salt = await bcrypt.genSalt();
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            } catch (err) {
+                return res.status(500).json(err);
+            }
+        }
+        try {
+            const user = await User.findByIdAndUpdate(req.params._id, {
+                $set: req.body,
+            });
+            res.status(200).json("account has been updated");
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("You can't update yourself");
+    }
+});
+
+//delete a user
+
+app.delete(
+    "/users/:_id",
+    async (req: express.Request, res: express.Response) => {
+        if (req.params._id === req.body._id || req.body.isAdmin) {
+            try {
+                await User.findByIdAndDelete(req.params._id);
+                res.status(200).json("account has been deleted");
+            } catch (err) {
+                return res.status(500).json(err);
+            }
+        } else {
+            return res.status(403).json("You can't delete yourself");
+        }
+    }
+);
+
+//get a User
+app.get("/users/:_id", async (req: express.Request, res: express.Response) => {
+    try {
+        const user = await User.findById(req.params._id);
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 app.get("/logout", (req: express.Request, res: express.Response) => {
     logAnonymousUserIn(req, res);
 });
